66d9ba041c7dd88cbb36e09f41e2d5d6506fb3cd
how-to-commit <SaltTheMail@protonmail.com>
2022-07-25 23:47:43 +0800

    update search.json

--- search/search.js
+++ search/search.js
@@ -1,3 +1,44 @@
+let sitemap = 
+{
+    "Main": {
+        "moves": [],
+        "link": "./index.html"
+    },
+
+    "Search": {
+        "moves": [],
+        "link": "./search/search.html"
+    },
+
+    "Scandinavian Defense": {
+        "moves": [
+            "e4", "d5", "exd5"
+        ],
+        "link": "tests"
+    },
+
+    "Queen's Gambit": {
+        "moves": [
+            "d4", "d5", "c4"
+        ],
+        "link": "testq"
+    }
+};
+
+for (page in sitemap) {
+    console.log(page);
+}
+
 function search(str) {
+    // filter input
+    str = str.trim().toLowerCase();
+
+    let results = [];
+    for (page in sitemap) {
+        if (page.toLowerCase().includes(str) || sitemap[page]["moves"].indexOf(str) != -1) {
+            results.push(page);
+        }
+    }
     
+    console.log(results);
 }
