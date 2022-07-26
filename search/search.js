b04cbcaba46c4063a0eebe04d049a5b3c7abf4d0
how-to-commit <SaltTheMail@protonmail.com>
2022-07-26 12:41:35 +0800

    complete search

--- search/search.js
+++ search/search.js
@@ -1,4 +1,4 @@
-let sitemap = 
+const sitemap = 
 {
     "Main": {
         "moves": [],
@@ -25,20 +25,51 @@ let sitemap =
     }
 };
 
-for (page in sitemap) {
-    console.log(page);
-}
 
 function search(str) {
     // filter input
     str = str.trim().toLowerCase();
 
-    let results = [];
-    for (page in sitemap) {
+    const results = [];
+    for (let page in sitemap) {
         if (page.toLowerCase().includes(str) || sitemap[page]["moves"].indexOf(str) != -1) {
             results.push(page);
         }
     }
-    
+
+    const resultSpan = document.getElementById("search-results");
+
+    // clear all results from previous search
+    resultSpan.textContent = '';
+    // horizontal rule to not look awkward
+    resultSpan.appendChild(document.createElement("hr"));
+
+    // loop through results to display
+    if (results.length == 0) {
+        // if no results
+        const resultPara = document.createElement("p");
+        resultPara.innerText = "Whoops! Nothing here. Try another name?"
+        resultSpan.appendChild(resultPara);
+
+        // hr
+        resultSpan.appendChild(document.createElement("hr"));
+
+    } else {
+        // there are results
+        for (let result of results) {
+            const resultPara = document.createElement("p");
+            const resultLink = document.createElement("a");
+            
+            // fill resultLink fields
+            resultLink.href = sitemap[result]["link"];
+            resultLink.innerText = result
+
+            resultPara.appendChild(resultLink);
+            resultSpan.appendChild(resultPara);
+
+            // horizontal rule
+            resultSpan.appendChild(document.createElement("hr"));
+        }
+    }
     console.log(results);
 }
