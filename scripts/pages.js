1c36cb6e0a4a94e5c70b226e0d0b495ca75c2f05
how-to-commit <SaltTheMail@protonmail.com>
2022-08-02 23:05:58 +0800

    refactor directory layout

--- scripts/pages.js
+++ scripts/pages.js
@@ -10,38 +10,45 @@ const pages =
         "link": "./search.html"
     },
 
+    "Fried Liver Attack": {
+        "moves": [
+            "e4", "e5", "Nf3", "Nc6", "Bc4", "Nf6", "Ng5"
+        ],
+        "link": "./openings/fried_liver_attack.html"
+    },
+
     "Italian Game": {
         "moves": [
             "e4", "e5", "Nf3", "Nc6", "Bc4"
         ],
-        "link": "./openings/italian_game/italian_game.html"
+        "link": "./openings/italian_game.html"
     },
 
     "Scandinavian Defense": {
         "moves": [
             "e4", "d5", "exd5", "Qa5", "Qd8"
         ],
-        "link": "./openings/scandinavian_defense/scandinavian.html"
+        "link": "./openings/scandinavian_defense.html"
     },
 
     "Scotch Game": {
         "moves": [
             "e4", "e5", "Nf3", "Nc6", "d4", "exd4", "Nxd4"
         ],
-        "link": "./openings/scotch_game/scotch_game.html"
+        "link": "./openings/scotch_game.html"
     },
 
     "Sodium Attack (Durkin Opening)": {
         "moves": [
             "Na3"
         ],
-        "link": "./openings/sodium_attack/sodium_attack.html"
+        "link": "./openings/sodium_attack.html"
     },
 
     "Two Knights Defense": {
         "moves": [
-            "e4", "e5", "Nf3", "Nc6",
+            "e4", "e5", "Nf3", "Nc6", "Bc4", "Nf6"
         ],
-        "link": "./openings/two_knights_defense/two_knights_defense.html"
+        "link": "./openings/two_knights_defense.html"
     },
 };
